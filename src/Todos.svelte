be364829aa3ed343bf75de3d1cf7120fc995e3d5
Tim Eitel <timeitel@outlook.com>
2020-08-01 22:49:43 +0800

    Add todo components

---
+++ src/Todos.svelte
@@ -0,0 +1,21 @@
+<script>
+  let todos = [
+    {
+      id: 1,
+      title: "My first todo",
+      completed: false,
+    },
+    {
+      id: 2,
+      title: "My second todo",
+      completed: false,
+    },
+    {
+      id: 3,
+      title: "My third todo",
+      completed: false,
+    },
+  ];
+</script>
+
+import TodoItem from './TodoItem.svelte';

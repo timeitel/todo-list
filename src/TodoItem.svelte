8a4941da051d859d7584a78550d3e7998629fef3
Tim Eitel <timeitel@outlook.com>
2020-08-02 16:20:13 +0800

    Emit events from child component

--- src/TodoItem.svelte
+++ src/TodoItem.svelte
@@ -1,3 +1,22 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+  import { fly } from "svelte/transition";
+  export let id;
+  export let title;
+  export let completed;
+  const dispatch = createEventDispatcher();
+  function deleteTodo() {
+    dispatch("deleteTodo", {
+      id: id,
+    });
+  }
+  function toggleComplete() {
+    dispatch("toggleComplete", {
+      id: id,
+    });
+  }
+</script>
+
 <div class="todo-item">
   <div class="todo-item-left" transition:fly={{ y: 20, duration: 300 }}>
     <input

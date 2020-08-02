ad2a36502b7f138bb6eb76d911d15ba86fdaeb32
Tim Eitel <timeitel@outlook.com>
2020-08-02 16:19:01 +0800

    Add todoitem component template

---
+++ src/TodoItem.svelte
@@ -0,0 +1,10 @@
+<div class="todo-item">
+  <div class="todo-item-left" transition:fly={{ y: 20, duration: 300 }}>
+    <input
+      type="checkbox"
+      bind:checked={completed}
+      on:change={toggleComplete} />
+    <div class="todo-item-label" class:completed>{title}</div>
+  </div>
+  <div class="remove-item" on:click={deleteTodo}>×</div>
+</div>

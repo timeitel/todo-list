f4a9830de7e62f52dd5b15fde8dda804e715f3b2
Tim Eitel <timeitel@outlook.com>
2020-08-02 16:16:03 +0800

    Add event functions

--- src/Todos.svelte
+++ src/Todos.svelte
@@ -1,8 +1,12 @@
 <script>
+  let newTodoTitle = "";
+  let currentFilter = "all";
+  let nextId = 4;
+
   let todos = [
     {
       id: 1,
-      title: "My first todo",
+      title: "First todo",
       completed: false,
     },
     {
@@ -16,6 +20,113 @@
       completed: false,
     },
   ];
+
+  function addTodo(event) {
+    if (event.key === "Enter") {
+      todos = [
+        ...todos,
+        {
+          id: nextId,
+          completed: false,
+          title: newTodoTitle,
+        },
+      ];
+      nextId = nextId + 1;
+      newTodoTitle = "";
+    }
+  }
+
+  $: todosRemaining = filteredTodos.filter((todo) => !todo.completed).length;
+  $: filteredTodos =
+    currentFilter === "all"
+      ? todos
+      : currentFilter === "completed"
+      ? todos.filter((todo) => todo.completed)
+      : todos.filter((todo) => !todo.completed);
+
+  function checkAllTodos(event) {
+    todos.forEach((todo) => (todo.completed = event.target.checked));
+    todos = todos;
+  }
+
+  function updateFilter(newFilter) {
+    currentFilter = newFilter;
+  }
+
+  function clearCompleted() {
+    todos = todos.filter((todo) => !todo.completed);
+  }
+
+  // handle emitted events from todoitem component
+  function handleDeleteTodo(event) {
+    todos = todos.filter((todo) => todo.id !== event.detail.id);
+  }
+  function handleToggleComplete(event) {
+    const todoIndex = todos.findIndex((todo) => todo.id === event.detail.id);
+    const updatedTodo = {
+      ...todos[todoIndex],
+      completed: !todos[todoIndex].completed,
+    };
+    todos = [
+      ...todos.slice(0, todoIndex),
+      updatedTodo,
+      ...todos.slice(todoIndex + 1),
+    ];
+  }
 </script>
 
+<div class="container">
+  <a href="https://codingthesmartway.com" target="_blank">
+    <img src={'/img/CTSWLogo.png'} alt="svelte logo" class="logo" />
+  </a>
+  <h2>Svelte Todo App</h2>
+  <input
+    type="text"
+    class="todo-input"
+    placeholder="Insert todo item ..."
+    bind:value={newTodoTitle}
+    on:keydown={addTodo} />
+  {#each filteredTodos as todo}
+    <div class="todo-item">
+      <TodoItem
+        {...todo}
+        on:deleteTodo={handleDeleteTodo}
+        on:toggleComplete={handleToggleComplete} />
+    </div>
+  {/each}
+  <div class="inner-container">
+    <div>
+      <label>
+        <input
+          class="inner-container-input"
+          type="checkbox"
+          on:change={checkAllTodos} />
+        Check All
+      </label>
+    </div>
+    <div>{todosRemaining} items left</div>
+  </div>
+  <div class="inner-container">
+    <div>
+      <button
+        on:click={() => updateFilter('all')}
+        class:active={currentFilter === 'all'}>
+        All
+      </button>
+      <button
+        on:click={() => updateFilter('active')}
+        class:active={currentFilter === 'active'}>
+        Active
+      </button>
+      <button
+        on:click={() => updateFilter('completed')}
+        class:active={currentFilter === 'completed'}>
+        Completed
+      </button>
+    </div>
+    <div>
+      <button on:click={clearCompleted}>Clear Completed</button>
+    </div>
+  </div>
+</div>
 import TodoItem from './TodoItem.svelte';
